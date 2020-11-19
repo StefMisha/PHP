3859d3cd63e251d0a725fc5daeaf150adf018156
StefMisha <stefmisha@gmail.com>
2020-11-19 14:30:06 +0300

    Комментарии и каталог сделаны

--- Lesson-6/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-const SERVER = "localhost";
-const DB = "november9";
-const LOGIN = "root";
-const PASS = "root";
-
-$connect = mysqli_connect(SERVER, LOGIN, PASS, DB) or die("Ошибка при коннекте к базе данных");
